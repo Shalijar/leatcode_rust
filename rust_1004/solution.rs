107b1b76cc3fda3ad6065c41a93954a81292868b
Shalijar <ali0shajari@gmail.com>
2023-06-26 21:19:10 -0400

    Added solution for problem 1004

---
+++ rust_1004/solution.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn longest_ones(nums: Vec<i32>, k: i32) -> i32 {
+        let (mut output, mut temp, mut temp_k) = (0, 0, 0);
+        let mut left = 0;
+        for i in 0..nums.len() {
+            if nums[i] == 0 {
+                temp_k += 1;
+            }
+            if temp_k > k {
+                if nums[left] == 0 {
+                    temp_k -= 1;
+                }
+                left += 1;
+            } 
+            output = output.max(i - left + 1);
+        }
+        output as i32
+    }
+}
